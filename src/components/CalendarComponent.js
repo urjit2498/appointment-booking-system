910287717692a8d784fd94f41ffbd88cf4fc0233
urjit2498 <urjit64@gmail.com>
2024-01-27 03:30:20 +0530

    Calendar setup with basic design

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,7 +1,24 @@
 import React from "react";
+import '../styles/Calendar.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
 const CalendarComponent = () => {
   return (
-    <div>CalendarComponent</div>
+    <div className="calendar_main_div">
+      <div className="calendar">
+        <div className="calendar_header">
+          <h3>Select Dates</h3>
+          <span>Timezone: Asia/Calcutta</span>
+        </div>
+        <div className="calendar_body">
+          <Calendar />
+        </div>
+      </div>
+      <div className="slots">
+        <p>slots</p>
+      </div>
+    </div>
   );
 };
 
